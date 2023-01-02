dbc1a69d91e735168a5962fe1ec8b8e7cb2f9252
Cheikh <cheikhbeoumar@gmail.com>
2023-01-02 07:34:13 -0800

    csrf setting

--- getarchive/settings.py
+++ getarchive/settings.py
@@ -124,3 +124,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CSRF_TRUSTED_ORIGINS = ['https://getarchive.up.railway.app','https://127.0.0.1:8000']
